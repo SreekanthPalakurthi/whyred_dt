fdec659ffc7e725bc01556f1ab5a9a7f495b7a6a
sreekfreak995 <sreekfreak995@gmail.com>
2019-11-17 00:14:10 +0530

    Revert "Update device.mk"
    This reverts commit 908ad82ed555db9964455d829be0234a68abb8f8.

--- device.mk
+++ device.mk
@@ -324,7 +324,7 @@ PRODUCT_PACKAGES += \
 
     android.hardware.power@1.2-service-qti \
     android.hardware.power.stats@1.0-service.mock
-    
+    android.hardware.power@1.2-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
