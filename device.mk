91adad60b96abb5e712e5733b6cdb029b199160f
sreekfreak995 <sreekfreak995@gmail.com>
2019-11-19 20:44:50 +0530

    Revert "whyred: build power stats hal"
    This reverts commit c7ce5ff76223a87b04b92e3b5a5fc81923aebff5.

--- device.mk
+++ device.mk
@@ -324,9 +324,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
