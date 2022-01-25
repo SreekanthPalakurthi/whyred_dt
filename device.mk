9ed6c12ee1dc5066c4b916dbff586109372b29d9
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2022-01-26 00:23:40 +0530

    whyred: Import qcom thermal HAL
    * From and till - https://github.com/ArrowOS/android_hardware_qcom_thermal/tree/7a7d30ea6db6a8e302cd60243d8c80a2628748db
    
    Change-Id: Ie8419f5f51fc4b9ef4fe21b7f32d325542b9a34e

--- device.mk
+++ device.mk
@@ -507,7 +507,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal@2.0-service.whyred
 
 # Touchscreen
 PRODUCT_PACKAGES += \
